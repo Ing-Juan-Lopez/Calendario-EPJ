a3a30a8115ff7090f987a54acf8d53e8085cff2d
Ing-Juan-Lopez <ing.juan.9652@gmail.com>
2026-06-02 17:20:50 -0500

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -15,8 +15,15 @@ const eventosDelCalendario = [
     { fecha: "2026-06-02", titulo: "Cumpleaños de Victor Alfonso Romero León", tipo: "cumple" },
     { fecha: "2026-06-03", titulo: "Cumpleaños de Juan Pablo López Cuéllar", tipo: "cumple" },
     { fecha: "2026-06-07", titulo: "Formación mensual de junio", tipo: "formacion" },
+    { fecha: "2026-06-28", titulo: "Visita Comité Promotor Nacional", tipo: "comite" },
+    { fecha: "2026-07-25", titulo: "RCPN Yarumal", tipo: "comite" },
+    { fecha: "2026-07-26", titulo: "RCPN Yarumal", tipo: "comite" },
     { fecha: "2026-08-31", titulo: "Cumpleaños de Jonathan Gilberto Hernández Vasquez", tipo: "cumple" },
     { fecha: "2026-10-03", titulo: "Cumpleaños del Padre Carlos Julio Beltrán Gutiérrez", tipo: "cumple" },
+    { fecha: "2026-10-09", titulo: "Encuentro 83H y 86M", tipo: "retiro" },
+    { fecha: "2026-10-10", titulo: "Encuentro 83H y 86M", tipo: "retiro" },
+    { fecha: "2026-10-11", titulo: "Encuentro 83H y 86M", tipo: "retiro" },
+    { fecha: "2026-10-12", titulo: "Encuentro 83H y 86M", tipo: "retiro" },
     { fecha: "2026-11-06", titulo: "Cumpleaños de Juliana Pabón Beltrán", tipo: "cumple" },
     { fecha: "2026-11-21", titulo: "Cumpleaños de Cristian Camilo Herrera Herrera", tipo: "cumple" }
 ];
